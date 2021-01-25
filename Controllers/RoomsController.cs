ad4b00aa3c7a4b0fcd17ba18d0b508adaad176ad
Simon van der Weele <vanderweelesimon@gmail.com>
2021-01-25 18:53:54 +0100

    More CRUD Added Patch and Delete Endpoints

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Motel.Data;
 using Motel.Dtos;
@@ -71,6 +72,47 @@ namespace Motel.Controllers
             await _repository.UpdateRoom(room);
             await _repository.SaveChanges();
 
+            return NoContent();
+        }
+
+
+        //PATCH api/rooms/{id}
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> PartialRoomUpdate(int id, JsonPatchDocument<RoomUpdateDto> patchDocument)
+        {
+            var roomFromRepo = await _repository.GetRoomById(id);
+            if (roomFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            var roomToPatch = _mapper.Map<RoomUpdateDto>(roomFromRepo);
+            patchDocument.ApplyTo(roomToPatch, ModelState);
+
+            if (!TryValidateModel(roomToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(roomToPatch, roomFromRepo);
+            await _repository.UpdateRoom(roomFromRepo);
+            await _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE api/rooms/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteRoom(int id)
+        {
+            var roomFromRepo = await _repository.GetRoomById(id);
+            if (roomFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.DeleteRoom(roomFromRepo);
+            await _repository.SaveChanges();
             return NoContent();
         }
 
